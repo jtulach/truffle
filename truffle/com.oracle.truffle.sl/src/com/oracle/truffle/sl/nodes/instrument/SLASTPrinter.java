67abd39069ec7170ee427c8ec996b25b65e248a1
Jaroslav Tulach <jaroslav.tulach@oracle.com>
2016-02-12 14:49:08 +0100

    Merge changes from #45, which removed SL dependency on an implementation class for AST visualization.

--- truffle/com.oracle.truffle.sl/src/com/oracle/truffle/sl/nodes/instrument/SLASTPrinter.java
+++ truffle/com.oracle.truffle.sl/src/com/oracle/truffle/sl/nodes/instrument/SLASTPrinter.java
@@ -1,28 +1,44 @@
 /*
- * Copyright (c) 2014, 2015, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2012, 2014, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
- * This code is free software; you can redistribute it and/or modify it
- * under the terms of the GNU General Public License version 2 only, as
- * published by the Free Software Foundation.  Oracle designates this
- * particular file as subject to the "Classpath" exception as provided
- * by Oracle in the LICENSE file that accompanied this code.
+ * The Universal Permissive License (UPL), Version 1.0
  *
- * This code is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
- * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
- * version 2 for more details (a copy is included in the LICENSE file that
- * accompanied this code).
+ * Subject to the condition set forth below, permission is hereby granted to any
+ * person obtaining a copy of this software, associated documentation and/or
+ * data (collectively the "Software"), free of charge and under any and all
+ * copyright rights in the Software, and any and all patent rights owned or
+ * freely licensable by each licensor hereunder covering either (i) the
+ * unmodified Software as contributed to or provided by such licensor, or (ii)
+ * the Larger Works (as defined below), to deal in both
  *
- * You should have received a copy of the GNU General Public License version
- * 2 along with this work; if not, write to the Free Software Foundation,
- * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ * (a) the Software, and
  *
- * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
- * or visit www.oracle.com if you need additional information or have any
- * questions.
+ * (b) any piece of software and/or hardware listed in the lrgrwrks.txt file if
+ * one is included with the Software each a "Larger Work" to which the Software
+ * is contributed by such licensors),
+ *
+ * without restriction, including without limitation the rights to copy, create
+ * derivative works of, display, perform, and distribute the Software and make,
+ * use, sell, offer for sale, import, export, have made, and have sold the
+ * Software and the Larger Work(s), and to sublicense the foregoing rights on
+ * either these or other terms.
+ *
+ * This license is subject to the following condition:
+ *
+ * The above copyright notice and either this complete permission notice or at a
+ * minimum a reference to the UPL must be included in all copies or substantial
+ * portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
  */
-package com.oracle.truffle.api.instrument.impl;
+package com.oracle.truffle.sl.nodes.instrument;
 
 import com.oracle.truffle.api.instrument.ASTPrinter;
 import com.oracle.truffle.api.instrument.InstrumentationNode;
@@ -38,11 +54,76 @@ import java.io.StringWriter;
 import java.util.ArrayList;
 
 /**
- * A language-agnostic for printing out various pieces of a Truffle AST.
+ * SLASTPrinter is used to print for SL's internal Truffle AST. This is used by
+ * {@link SLDefaultVisualizer} to provide a means of displaying the internal Truffle AST
  */
-public class DefaultASTPrinter implements ASTPrinter {
+public final class SLASTPrinter implements ASTPrinter {
 
-    public DefaultASTPrinter() {
+    public SLASTPrinter() {
+    }
+
+    protected void printTree(PrintWriter p, Node node, int maxDepth, Node markNode, int level) {
+        if (node == null) {
+            p.print("null");
+            return;
+        }
+
+        p.print(nodeName(node));
+
+        p.print("(");
+
+        if (node instanceof InstrumentationNode) {
+            p.print(instrumentInfo((InstrumentationNode) node));
+        }
+
+        p.print(sourceInfo(node));
+
+        p.print(NodeUtil.printSyntaxTags(node));
+
+        ArrayList<NodeFieldAccessor> childFields = new ArrayList<>();
+
+        for (NodeFieldAccessor field : NodeClass.get(node.getClass()).getFields()) {
+            if (field.getKind() == NodeFieldKind.CHILD || field.getKind() == NodeFieldKind.CHILDREN) {
+                childFields.add(field);
+            } else if (field.getKind() == NodeFieldKind.DATA) {
+                // p.print(sep);
+                // sep = ", ";
+                //
+                // final String fieldName = field.getName();
+                // switch (fieldName) {
+                //
+                // }
+                // p.print(fieldName);
+                // p.print(" = ");
+                // p.print(field.loadValue(node));
+            }
+        }
+        p.print(")");
+
+        if (level <= maxDepth) {
+
+            if (childFields.size() != 0) {
+                p.print(" {");
+                for (NodeFieldAccessor field : childFields) {
+
+                    Object value = field.loadValue(node);
+                    if (value == null) {
+                        printNewLine(p, level);
+                        p.print(field.getName());
+                        p.print(" = null ");
+                    } else if (field.getKind() == NodeFieldKind.CHILD) {
+                        printChild(p, maxDepth, markNode, level, field, value);
+                    } else if (field.getKind() == NodeFieldKind.CHILDREN) {
+                        printChildren(p, maxDepth, markNode, level, field, value);
+                    } else {
+                        printNewLine(p, level);
+                        p.print(field.getName());
+                    }
+                }
+                printNewLine(p, level - 1);
+                p.print("}");
+            }
+        }
     }
 
     @Override
@@ -85,70 +166,6 @@ public class DefaultASTPrinter implements ASTPrinter {
             sb.append(NodeUtil.printSyntaxTags(wrapper));
         }
         return sb.toString();
-    }
-
-    protected void printTree(PrintWriter p, Node node, int maxDepth, Node markNode, int level) {
-        if (node == null) {
-            p.print("null");
-            return;
-        }
-
-        p.print(nodeName(node));
-
-        p.print("(");
-
-        if (node instanceof InstrumentationNode) {
-            p.print(instrumentInfo((InstrumentationNode) node));
-        }
-
-        p.print(sourceInfo(node));
-
-        p.print(NodeUtil.printSyntaxTags(node));
-
-        ArrayList<NodeFieldAccessor> childFields = new ArrayList<>();
-
-        for (NodeFieldAccessor field : NodeClass.get(node).getFields()) {
-            if (field.getKind() == NodeFieldKind.CHILD || field.getKind() == NodeFieldKind.CHILDREN) {
-                childFields.add(field);
-            } else if (field.getKind() == NodeFieldKind.DATA) {
-                // p.print(sep);
-                // sep = ", ";
-                //
-                // final String fieldName = field.getName();
-                // switch (fieldName) {
-                //
-                // }
-                // p.print(fieldName);
-                // p.print(" = ");
-                // p.print(field.loadValue(node));
-            }
-        }
-        p.print(")");
-
-        if (level <= maxDepth) {
-
-            if (childFields.size() != 0) {
-                p.print(" {");
-                for (NodeFieldAccessor field : childFields) {
-
-                    Object value = field.loadValue(node);
-                    if (value == null) {
-                        printNewLine(p, level);
-                        p.print(field.getName());
-                        p.print(" = null ");
-                    } else if (field.getKind() == NodeFieldKind.CHILD) {
-                        printChild(p, maxDepth, markNode, level, field, value);
-                    } else if (field.getKind() == NodeFieldKind.CHILDREN) {
-                        printChildren(p, maxDepth, markNode, level, field, value);
-                    } else {
-                        printNewLine(p, level);
-                        p.print(field.getName());
-                    }
-                }
-                printNewLine(p, level - 1);
-                p.print("}");
-            }
-        }
     }
 
     protected void printChildren(PrintWriter p, int maxDepth, Node markNode, int level, NodeFieldAccessor field, Object value) {
