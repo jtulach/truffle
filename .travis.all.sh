9f33a8345b6cf25791d9dc179225d9ff7aaf32cc
Jaroslav Tulach <jaroslav.tulach@oracle.com>
2016-04-25 14:41:27 +0200

    Use JRuby as binary dependency

--- .travis.all.sh
+++ .travis.all.sh
@@ -4,9 +4,10 @@ cp -r .git * mxbuild/all/truffle/
 cd mxbuild/all
 hg clone http://graal.us.oracle.com/hg/graalvm/
 cd graalvm
+hg up -C mx4ruby
 
 export DEFAULT_VM=server
-export MX_BINARY_SUITES=graal-enterprise,jvmci,graal-js-extensions,graal-core,graal-js,graal-avatarjs,fastr
+export MX_BINARY_SUITES=graal-enterprise,jvmci,graal-js-extensions,graal-core,graal-js,graal-avatarjs,fastr,jruby
 
 mx --vm server --vmbuild product build
 mx unittest interop.sieve
