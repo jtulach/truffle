c15b9318d4f4aa4922a3fa793a21025cc2d7d154
Jaroslav Tulach <jaroslav.tulach@oracle.com>
2016-05-27 15:36:01 +0200

    Using the proposed 'mx sanitycheck' command to build graalvm against the most recent truffle

--- .travis.all.sh
+++ .travis.all.sh
@@ -1,14 +1,23 @@
 #!/bin/bash
-mkdir -p mxbuild/all/truffle
-cp -r .git * mxbuild/all/truffle/
-cd mxbuild/all
-hg clone http://graal.us.oracle.com/hg/graalvm/
-cd graalvm
-hg up -C mx4ruby
 
+if [ -e mxbuild/mx ]; then
+  cd mxbuild/mx
+  git pull
+else
+  mkdir -p mxbuild
+  cd mxbuild
+  git clone ssh://ol-bitbucket.us.oracle.com:7999/~jaroslav.tulach_oracle.com/mx.git
+  cd mx
+fi
+git checkout SanityCheck
+
+PATH=`pwd`:$PATH
+cd ../..
+
+export SANITYCHECK_URL=ssh://git@ol-bitbucket.us.oracle.com:7999/g/graalvm.git
 export DEFAULT_VM=server
-export MX_BINARY_SUITES=graal-enterprise,jvmci,graal-js-extensions,graal-core,graal-js,graal-avatarjs,fastr,jruby
+#export MX_BINARY_SUITES=graal-enterprise,jvmci,graal-js-extensions,graal-core,graal-js,graal-avatarjs,fastr,jruby
 
-mx -v --vm server --vmbuild product build
-mx unittest interop.sieve
+mx sanitycheck -v --vm server --vmbuild product build
+mx sanitycheck unittest interop.sieve
 
